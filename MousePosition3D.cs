4ad813b381f44dff86539c89d29f1fd49c5a0ae6
Travis <82448667+TravisOnline@users.noreply.github.com>
2022-02-11 07:57:46 +0000

    Update MousePosition3D.cs

--- MousePosition3D.cs
+++ MousePosition3D.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+* Place this script on an object that will follow the player mouse
+* in a 3D environment.
+* the Layermask array should be set to everything but the parent object's layer and
+* possibly the camera/anything the camera is attached to.
+*/
+
 public class MousePosition3D : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private LayerMask surfaces;
-    public bool canBuildMouse;
 
     private void Start()
     {
@@ -18,13 +24,7 @@ public class MousePosition3D : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if(Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, surfaces))
         {
-            canBuildMouse = true;
             transform.position = raycastHit.point;
-            //Need to calculate position from hit object, add or remove by 1
-        }
-        else
-        {
-            canBuildMouse = false;
         }
     }
 }
